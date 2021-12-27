9362c4fde9b6dc81c7b110774c4bf831076402aa
Everton Andrade <everton@andrade.mat.br>
2021-12-27 11:20:33 -0400

    feat: added build

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
+  output: {
+    filename: 'restate.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      name: 'restate',
+      type: 'umd',
+    },
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({
+      terserOptions: {
+        format: {
+          comments: false,
+        },
+      },
+      extractComments: false,
+    })],
+  },
+};
