453ac5e0e94f9b80767637802dc16bfd326275e7
Everton Andrade <everton@andrade.mat.br>
2022-06-10 08:08:33 -0400

    feat

--- package/createRestate.d.ts
+++ package/createRestate.d.ts
@@ -1,5 +1,7 @@
 import type { IHTTPClient, Restate, IStore } from ".";
 
+export declare const $restate: { instance?: Restate };
+
 export declare function createRestate(provider: {
   httpClient: IHTTPClient;
   store: IStore;
