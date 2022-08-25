28a823ad235f341f7183c7fd716b1f2751897da4
Everton Andrade <everton@andrade.mat.br>
2022-08-25 14:19:52 -0400

    build: release v1.0.0-alpha.17.0

---
+++ package/models/CollectionModel.d.ts
@@ -0,0 +1,53 @@
+import type { ComputedRef, Ref } from "vue";
+
+import { CoreModel } from "./CoreModel";
+
+export interface BaseOptions {
+  query?: Record<string, string>;
+}
+
+export interface IndexOptions extends BaseOptions {
+  merge?: boolean;
+}
+
+export type ShowOptions = BaseOptions;
+
+export type StoreOptions = BaseOptions;
+
+export type UpdateOptions = BaseOptions;
+
+export type DestroyOptions = BaseOptions;
+
+export type LoadWithData<D> = Load & { data: D };
+
+export declare class CollectionModel<RI> extends CoreModel<RI> {
+  public $pk = "id";
+
+  constructor(public $resourceName: string, public $axios: Axios);
+
+  public data(): ComputedRef<Partial<RI>[]>;
+
+  public item(id: string | number): Ref<Partial<RI>>;
+
+  public index(
+    options?: IndexOptions
+  ): LoadWithData<ComputedRef<Partial<RI>[]>>;
+
+  public show(
+    id: string | number,
+    options?: ShowOptions
+  ): LoadWithData<Ref<Partial<RI> | Record<string, never>>>;
+
+  public store(
+    payload: Partial<RI>,
+    options?: StoreOptions
+  ): LoadWithData<ComputedRef<Partial<RI> | null>>;
+
+  public update(
+    id: string | number,
+    data: Partial<RI>,
+    options?: UpdateOptions
+  ): Load;
+
+  public destroy(id: string | number, options?: DestroyOptions): Load;
+}
