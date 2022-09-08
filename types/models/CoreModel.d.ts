bfc31781e570b811ba8599e213ea11e01ffd5a6b
Everton Andrade <everton@andrade.mat.br>
2022-09-08 07:19:23 -0400

    fix

--- types/models/CoreModel.d.ts
+++ types/models/CoreModel.d.ts
@@ -6,9 +6,4 @@ export declare class CoreModel<RI> {
   public $resource: Resource<RI>;
 
   constructor(public $resourceName: string);
-
-  public load(request: () => Promise<void>): {
-    loaded: Promise<boolean>;
-    loading: Ref<boolean>;
-  };
 }
